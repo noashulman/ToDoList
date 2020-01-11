af5ca1a8f1aa1430c6a0d79c41df5ea1675b854e
Noa Shulman <noa.shulman@mail.huji.ac.il>
2020-01-11 18:08:11 +0200

    added draggability to list

--- index.js
+++ index.js
@@ -13,54 +13,82 @@ class ToDo {
 
 writeDown = () => {
     const item = document.createElement("div");
-    item.className="item"
+    item.className = "item"
+    item.classList.add("list-group-item")
     listOf.appendChild(item);
 
     const itemText = document.createElement("div");
     itemText.classList.add("item-text");
-    itemText.contentEditable="true";
+    itemText.contentEditable = "true";
     let userDescription = document.querySelector("#text").value;
     const li = new ToDo(userDescription)
     console.log(li.description);
-    document.querySelector("#text").value=""
-   
+    document.querySelector("#text").value = ""
+
     itemText.innerHTML = `${li.description}`;
     const can = document.createElement("div");
-    can.className="smallbtn can far fa-trash-alt"
-    
+    can.className = "smallbtn can far fa-trash-alt"
+
     const vee = document.createElement("div");
-    vee.className="smallbtn vee far fa-circle"
-    
+    vee.className = "smallbtn vee far fa-circle"
+
     item.appendChild(can);
     item.appendChild(vee);
     item.appendChild(itemText);
 
-    
+
 
     const cans = document.querySelectorAll(".can");
     cans.forEach(can => {
-    can.addEventListener("click", deleteItem)
-    can.style.backgroundColor = "green"
-    
-    console.log(cans);
-    
-   
-});
+        can.addEventListener("click", deleteItem)
+        
+
+        
+    });
+
+    const vees = document.querySelectorAll(".vee");
+    vees.forEach(vee => {
+        vee.addEventListener("click", completed)
+        
+        
+    });
+
+    Sortable.create(simpleList,{
+        animation:150,
+        delayOnTouchOnly:true,
+        ghostClass: "ghost"
+    })
 
 }
 
-deleteItem = (event) =>{
+deleteItem = (event) => {
     event.target.parentNode.parentNode.removeChild(event.target.parentNode);
 
 
 }
+
+completed =(event) =>{
+    console.log(event.target);
+    if (flagComplete===true){
+    event.target.className = " smallbtn vee far fa-check-circle"
+    event.target.nextElementSibling.style.textDecorationLine="line-through"
+    flagComplete = false
+    }else {
+        event.target.className = " smallbtn vee far fa-circle"
+    event.target.nextElementSibling.style.textDecorationLine="none"
+    flagComplete = true
+    }
+    
+    
+}
+
 const doIt = document.querySelector(".btn");
 doIt.addEventListener("click", writeDown);
 
 const listOf = document.querySelector(".list")
 
 
-let flag = true;
+let flagComplete = true;
 
 
 
